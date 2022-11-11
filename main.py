4abc895e21264c36ce31b6ef96a5f8b0a2970c15
임재용 <limjaeyong@imjaeyong-ui-MacBookAir.local>
2022-11-11 23:41:55 +0900

    first commit of game, Create raycasting.py main.py map1.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame
+import sys
+from map1 import *
+RES = WIDTH, HEIGHT = 1920, 1080
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(RES)
+        self.clock = pygame.time.Clock()
+        self.new_game()
+        
+    def new_game(self):
+        self.map = Map(self)
+    
+    def update(self):
+        pygame.display.flip()
+        self.clock.tick(0)
+        pygame.display.set_caption(f'{self.clock.get_fps() :.1f}')
+        
+    def draw(self):
+        self.screen.fill('black')
+        self.map.draw()
+    
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+                
+                
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+            
+            
+if __name__ == '__main__':
+    game = Game()
+    game.run()
